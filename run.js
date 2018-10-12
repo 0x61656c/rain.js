164c890a7d2ec0c952ec1ab75ac4a8da57e92f60
alebml <37777357+alebml@users.noreply.github.com>
2018-10-11 23:08:52 -0400

    Add files via upload

--- run.js
+++ run.js
@@ -9,9 +9,19 @@ document.body.appendChild(canvas);
 //Then you can draw a point at (10,10) like this:
 
 var ctx = canvas.getContext("2d");
-var x_size = 10;
-var y_size = 10;
+
+//Temporary setting for test
+ var my_gradient=ctx.createLinearGradient(0,0,0,170);
+my_gradient.addColorStop(0,"white");
+my_gradient.addColorStop(1,"blue");
+ctx.fillStyle=my_gradient;
+
+var x_size = 1;
+var y_size = 2;
 var particleArray = [];
+var max_height = 1000;
+var cloud_bottom = 10;
+var max_width = 2000;
 
 class Particle {
 
@@ -21,7 +31,12 @@ class Particle {
   }
 
   fall(){
-  	this.y  = this.y + 1;
+    if (this.y < max_height){this.y++}
+    else {
+      this.y = 0;
+      this.x = getRandomInt(max_width)
+    }
+
   }
   drawParticle(){
     ctx.fillRect(this.x,this.y,x_size,y_size);
@@ -32,21 +47,39 @@ let p1 = new Particle(10,10);
 let p2 = new Particle(100,20);
 //p1.drawParticle();
 
-function refreshCanvas(particle){
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function refreshCanvas(particle,speed){
   setInterval(function(){
-  ctx.clearRect(particle.x, particle.y - 1, x_size, y_size);
-  particle.fall();
-  particle.drawParticle();},5);
-  console.log(particle.y)
+    ctx.clearRect(particle.x, particle.y - 1, x_size, y_size);
+    particle.fall();
+    particle.drawParticle();},speed);
 }
 
 particleArray.push(p1);
-//particleArray.push(p2);
+particleArray.push(p2);
+
+function populateArray(array){
+  var long = getRandomInt(max_width);
+  var lat = getRandomInt(max_height);
+  let entry = new Particle(long,lat);
+  particleArray.push(entry);
+}
 
 function main(){
 
+  var count = 0;
+  while(count < 1000){
+    populateArray(particleArray)
+    console.log(particleArray)
+    count++
+  }
+
   for(i = 0; i < particleArray.length; i++){
-    refreshCanvas(particleArray[i])
+    _speed = Math.random() * 100;
+    refreshCanvas(particleArray[i],_speed)
     }
   }
 
